0a6a98bb6c0c44db8cb0bb0ed0f1eea6f2d3ca33
Kimbang1 <arto135@naver.com>
2025-04-07 11:28:11 +0900

    로그인 연결

--- src/pages/header/Header.js
+++ src/pages/header/Header.js
@@ -1,22 +1,31 @@
 import React from "react";
-import {HeaderRoot} from "./Header.styled"
+import { HeaderRoot } from "./Header.styled";
 import { Text } from "../../common/Typography";
 import { ReactComponent as Logo } from "../../assets/images/Logo.svg";
 import { useNavigate } from "react-router-dom";
+import useKakaoLogin from "../../hooks/useKakaoLogin";
 
 const Header = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const { kakaoLogin } = useKakaoLogin();
 
-    const goToMain = () => {
-        navigate("/");
-    };
+  const goToMain = () => {
+    navigate("/");
+  };
 
-    return (
-        <HeaderRoot>
-            <Logo onClick={goToMain} />
-            <Text size="sm" $weight={600} style={{cursor: 'pointer'}}>로그인</Text>
-        </HeaderRoot>
-    );
+  return (
+    <HeaderRoot>
+      <Logo onClick={goToMain} />
+      <Text
+        size="sm"
+        $weight={600}
+        style={{ cursor: "pointer" }}
+        onClick={kakaoLogin}
+      >
+        로그인
+      </Text>
+    </HeaderRoot>
+  );
 };
 
 export default Header;
