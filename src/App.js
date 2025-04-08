ac119690e8c748be074cc1bee3181b6b4cbdad3b
Kimbang1 <arto135@naver.com>
2025-04-08 16:33:16 +0900

    라우트 수정

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Header from './pages/header/Header';
-import Home from './pages/home/Home';
-import FurnitureEditorPage from './pages/3d/FurnitureEditorPage'; // ✅ 태원
-import AISearchComponent from './pages/furnitureSearch/SearchPage';
+import Header from "./pages/header/Header";
+import Home from "./pages/home/Home";
+import FurnitureEditorPage from "./pages/3d/FurnitureEditorPage"; // ✅ 태원
+import AISearchComponent from "./pages/furnitureSearch/SearchPage";
 import MyRoom from "./pages/myroom/MyRoom";
+import KakaoCallback from "./pages/auth/KakaoCallback";
 
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-      <BrowserRouter>
-          <Header />
-          <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/placement/add" element={<FurnitureEditorPage />} /> {/* ✅ 태원 */}
-              <Route path="/search" element={<AISearchComponent />} />
-              <Route path="/myroom" element={<MyRoom />} />
-          </Routes>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/placement/add" element={<FurnitureEditorPage />} />{" "}
+        {/* ✅ 태원 */}
+        <Route path="/search" element={<AISearchComponent />} />
+        <Route path="/myroom" element={<MyRoom />} />
+        <Route path="/auth/callback/kakao" element={<KakaoCallback />} />
+      </Routes>
 
-          {/* 토스트 메시지 */}
-          <ToastContainer
-              position="top-center"
-              autoClose={5000}
-              hideProgressBar
-              closeButton={false}
-              toastClassName="custom-toast-body"
-          />
-      </BrowserRouter>
-
+      {/* 토스트 메시지 */}
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar
+        closeButton={false}
+        toastClassName="custom-toast-body"
+      />
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,28 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import theme from './theme/theme';
-import { ThemeProvider as StyledThemeProvider } from 'styled-components';
-import { ThemeProvider as MUIThemeProvider } from '@mui/material/styles';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import theme from "./theme/theme";
+import { ThemeProvider as StyledThemeProvider } from "styled-components";
+import { ThemeProvider as MUIThemeProvider } from "@mui/material/styles";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-
-    <React.StrictMode>
-        <Provider store={store}>
-            <MUIThemeProvider theme={theme}>
-                <StyledThemeProvider theme={theme}>
-                    <App />
-                </StyledThemeProvider>
-            </MUIThemeProvider>
-        </Provider>
-    </React.StrictMode>
-
+  <Provider store={store}>
+    <MUIThemeProvider theme={theme}>
+      <StyledThemeProvider theme={theme}>
+        <App />
+      </StyledThemeProvider>
+    </MUIThemeProvider>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
